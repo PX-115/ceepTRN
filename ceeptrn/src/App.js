d75bbdd872b936539d2d49abe223fedaa3454659
Vitor Hugo Pereira Silva <vitor_hugo_ps@hotmail.com>
2022-03-08 15:18:08 -0300

    Preparando ambiente para a Lista de Categorias

--- ceeptrn/src/App.js
+++ ceeptrn/src/App.js
@@ -3,17 +3,36 @@ import "./assets/App.css";
 import "./assets/index.css";
 import CampoTexto from "./components/CampoTexto";
 import ListaItens from "./components/ListaItens";
+import ArrayDeCartas from "./dados/Cartas";
+import ArrayDeCategorias from "./dados/Categoria";
 
 class App extends Component {
   constructor() {
     super();
 
-    this.state = {
-      cartas: [],
-    };
+    this.cartas = new ArrayDeCartas();
+    this.categorias = new ArrayDeCategorias();
   }
 
-  // Método público pois eu passo ele para o CampoTexto usar
+  render() {
+    return (
+      <section className="conteudo">
+        <CampoTexto
+          enviarCarta={
+            this.enviarCarta.bind(
+              this
+            ) /* Injeção de dependência / Associando a propriedade enviarCarta criada aqui a função de enviar uma carta no CampoTexto*/
+          }
+        />
+        <ListaItens cartas={this.state.cartas} />
+      </section>
+    );
+  }
+}
+
+export default App;
+
+/* // Método público pois eu passo ele para o CampoTexto usar
   // Criando uma função para passar como propriedade / passando ela na linha: 30
   enviarCarta(titulo, destinatario, texto, remetente) {
     const novaCarta = { titulo, destinatario, texto, remetente };
@@ -23,16 +42,4 @@ class App extends Component {
       cartas: novoArrayCartas,
     };
     this.setState(novoEstadoArrayCartas);
-  }
-
-  render() {
-    return (
-      <section className="conteudo">
-        <CampoTexto enviarCarta={this.enviarCarta.bind(this) /* Injeção de dependência / Associando a propriedade enviarCarta criada aqui a função de enviar uma carta no CampoTexto*/} />
-        <ListaItens cartas={this.state.cartas} />
-      </section>
-    );
-  }
-}
-
-export default App;
+  } */
--- ceeptrn/src/components/CorpoItem/CorpoItem.jsx
+++ ceeptrn/src/components/CorpoItem/CorpoItem.jsx
@@ -1,18 +1,29 @@
 import React, { Component } from "react";
 import "./estilo.css";
+import { ReactComponent as DeleteSVG } from "../../assets/img/delete.svg";
 
 class CorpoItem extends Component {
+  apagar() {
+    const indice = this.props.indice;
+    this.props.deletar(indice);
+  }
+
   render() {
     return (
       <section className="corpo-item">
         <header className="corpo-item_cabecalho">
           <h3 className="corpo-item_titulo">{this.props.titulo}</h3>
+          <DeleteSVG>{this.apagar.bind(this)}</DeleteSVG>
         </header>
         <header>
-          <h4 className="corpo-item_subtitulo">Destinatário: {this.props.destinatario}</h4>
+          <h4 className="corpo-item_subtitulo">
+            Destinatário: {this.props.destinatario}
+          </h4>
         </header>
         <p className="corpo-item_texto">{this.props.texto}</p>
-        <footer className="corpo-item_rodape">Remetente: {this.props.remetente}</footer>
+        <footer className="corpo-item_rodape">
+          Remetente: {this.props.remetente}
+        </footer>
       </section>
     );
   }
